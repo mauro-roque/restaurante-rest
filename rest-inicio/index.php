9bde73cc002d1c08de1505ca3a1bbb44e8ae5bac
mauro-roque <128740268+mauro-roque@users.noreply.github.com>
2024-06-29 16:35:23 -0300

    up
    projeto em wordpress como CMS

---
+++ rest-inicio/index.php
@@ -0,0 +1,8 @@
+<?php get_header(); ?>
+	<?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
+    	<h2><?php the_title(); ?></h2>
+    	<div><?php the_content(); ?></div>
+	<?php endwhile; else : ?>
+    	<p><?php esc_html_e( 'Sorry, no posts matched your criteria.' ); ?></p>
+	<?php endif; ?>
+<?php get_footer(); ?>
